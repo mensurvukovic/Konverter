b185cb22d38125eb38e1c6595421b33823765c89
Mensur Vukovic <mensur.v@hotmail.com>
2018-02-08 11:28:41 +0100

    first commit

---
+++ src/com/konvertor/konvertor.java
@@ -0,0 +1,34 @@
+package com.konvertor;
+
+public class konvertor {
+	
+//	1kg = 2.2046 lbs
+//	1cm = 0.393701 inch
+//	fahrenheit = (celsius * 1.8) + 32
+		
+	
+	public void kgToLbs(double kg){
+		
+	}
+	
+	public void lbsToKg(double lbs){
+		
+	}
+	
+	public void cmToInch(double cm){
+		
+	}
+	
+	public void inchToCm(double Inch){
+		
+	}
+	
+	public void celsiusToFahrenheit(double celsius){
+		
+	}
+	
+	public void FahrenheitToCelsius(double fahrenheit){
+		
+	}
+
+}
---
+++ src/com/konvertor/konvertorMain.java
@@ -0,0 +1,11 @@
+package com.konvertor;
+
+public class konvertorMain {
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
